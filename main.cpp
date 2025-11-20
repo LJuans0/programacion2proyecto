25b516643b4b20aa594442d0dcad06a168b9929d
Juan Estrella <johnstar08082@gmail.com>
2025-11-20 00:54:12 -0500

    Prueba2

--- main.cpp
+++ main.cpp
@@ -1,8 +1,16 @@
+#include "clases.h"
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 using namespace std;
 
 int main()
 {
-    cout<<"hola :v";
+    vector<vector<string>> mapa;
+    int turnosbase=0;
+    mapa[0][0]="holaaa";
+    mapa[0][1]="holaaaperro";
+    cout<<mapa[0][1];
     return 0;
 }
